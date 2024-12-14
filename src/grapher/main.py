e3eefe49f4c279c9f939f34fd47c92e21a9fad8e
limjiyoon <luinpark@gmail.com>
2024-12-14 13:36:13 +0900

    FEATURE: basic fastapi server

---
+++ src/grapher/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def test():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
